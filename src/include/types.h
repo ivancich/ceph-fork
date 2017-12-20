da500db853f48f4d9c699c31fbe1e3c22c8481b5
J. Eric Ivancich <ivancich@redhat.com>
2017-12-20 13:52:57 -0500

    When units of digitial information are displayed, indicate error if their values are negative.
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>

--- src/include/types.h
+++ src/include/types.h
@@ -316,12 +316,17 @@ inline ostream& operator<<(ostream& out, const client_t& c) {
 
 struct prettybyte_t {
   uint64_t v;
+  bool error;
   // cppcheck-suppress noExplicitConstructor
-  prettybyte_t(uint64_t _v) : v(_v) {}
+  prettybyte_t(uint64_t _v) : v(_v), error(false) {}
+  prettybyte_t(int64_t _v) : v(_v), error(_v < 0) {}
 };
 
 inline ostream& operator<<(ostream& out, const prettybyte_t& b)
 {
+  if (b.error)
+    return out << "negative-error";
+
   uint64_t bump_after = 100;
   if (b.v > bump_after << 60)
     return out << (b.v >> 60) << " EB";    
@@ -340,12 +345,17 @@ inline ostream& operator<<(ostream& out, const prettybyte_t& b)
 
 struct si_t {
   uint64_t v;
+  bool error;
   // cppcheck-suppress noExplicitConstructor
-  si_t(uint64_t _v) : v(_v) {}
+  si_t(uint64_t _v) : v(_v), error(false) {}
+  si_t(int64_t _v) : v(_v), error(_v < 0) {}
 };
 
 inline ostream& operator<<(ostream& out, const si_t& b)
 {
+  if (b.error)
+    return out << "negative-error";
+
   uint64_t bump_after = 100;
   if (b.v > bump_after << 60)
     return out << (b.v >> 60) << "E";
@@ -364,12 +374,17 @@ inline ostream& operator<<(ostream& out, const si_t& b)
 
 struct pretty_si_t {
   uint64_t v;
+  bool error;
   // cppcheck-suppress noExplicitConstructor
-  pretty_si_t(uint64_t _v) : v(_v) {}
+  pretty_si_t(uint64_t _v) : v(_v), error(false) {}
+  pretty_si_t(int64_t _v) : v(_v), error(_v < 0) {}
 };
 
 inline ostream& operator<<(ostream& out, const pretty_si_t& b)
 {
+  if (b.error)
+    return out << "negative-error";
+
   uint64_t bump_after = 100;
   if (b.v > bump_after << 60)
     return out << (b.v >> 60) << " E";
@@ -388,12 +403,17 @@ inline ostream& operator<<(ostream& out, const pretty_si_t& b)
 
 struct kb_t {
   uint64_t v;
+  bool error;
   // cppcheck-suppress noExplicitConstructor
   kb_t(uint64_t _v) : v(_v) {}
+  kb_t(int64_t _v) : v(_v), error(_v < 0) {}
 };
 
 inline ostream& operator<<(ostream& out, const kb_t& kb)
 {
+  if (b.error)
+    return out << "negative-error";
+
   uint64_t bump_after = 100;
   if (kb.v > bump_after << 40)
     return out << (kb.v >> 40) << " PB";    
